8e7ab49e4cba5d18fd70a9e7d4b63b0783a4b44a
sohnOfGod <jsohn000@gmail.com>
2018-08-20 14:41:51 -0700

    initial commit

---
+++ src/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once('src/facebook/autoload.php');
+
+$fb = new Facebook\Facebook([
+    'app_id' => '358204998052801', // Replace {app-id} with your app id
+    'app_secret' => '95224c95fdf35d41be5a6e4ed04b1e78',
+    'default_graph_version' => 'v3.1',
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+$permissions = ['email']; // Optional permissions
+$loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
+
+echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+</body>
+</html>
+
